52844c3ec63c0fc505cd25c759fbcfcf07062d9f
FeltThrone18 <32143478+FeltThrone18@users.noreply.github.com>
2017-12-06 13:29:01 -0800

    Add files via upload

---
+++ highlights-979943632/fetchusers.php
@@ -0,0 +1,34 @@
+<?php
+        $connection = mysqli_connect("localhost", "Rahul", "123456");
+        if ($connection->connect_error) {
+            die("Connection failed: " . $connection->connect_error);
+            echo('connection to db failed');
+            echo($connection);
+            }
+
+            // Selecting Database
+            $db = mysqli_select_db($connection, "abc");
+            // SQL query to fetch information of registerd users and finds user match.
+            $query = mysqli_query($connection, "select username, firstname, lastname from userdata;");
+            $rows = mysqli_num_rows($query);
+            if ($rows > 0) {
+                echo("<table style=\"width:100%\">
+                          <tr>
+                            <th>Username</th>
+                            <th>Firstname</th>
+                            <th>Lastname</th>
+                          </tr>");
+                while ($user = $query->fetch_assoc()) {
+                    echo("<tr>
+                            <td>".$user['username']."</td>
+                            <td>".$user['firstname']."</td>
+                            <td>".$user['lastname']."</td>
+                          </tr>");
+                }
+                echo("</table>");
+            }
+            else{
+                echo("No Users @ Highlights Gaming");
+            }
+            mysqli_close($connection);
+      ?>
